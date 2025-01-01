96a733c89cc1191d878828361d763340075e8414
FJReyna <franciscorgs@hotmail.es>
2024-12-31 20:09:05 -0600

    Changed GeoItemModel to DefaultedModel

--- src/main/java/studio/axzet/primordialforces/geo/VoidmancerArmorRenderer.java
+++ src/main/java/studio/axzet/primordialforces/geo/VoidmancerArmorRenderer.java
@@ -2,13 +2,12 @@ package studio.axzet.primordialforces.geo;
 
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.model.DefaultedItemGeoModel;
-import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 import studio.axzet.primordialforces.PrimordialForces;
 import studio.axzet.primordialforces.item.custom.VoidmancerArmorItem;
 
 public class VoidmancerArmorRenderer extends GeoArmorRenderer<VoidmancerArmorItem> {
     public VoidmancerArmorRenderer() {
-        super(new VoidmancerArmorModel());
+        super(new DefaultedItemGeoModel<>(ResourceLocation.fromNamespaceAndPath(PrimordialForces.MOD_ID, "armor/voidmancer")));
     }
 }
