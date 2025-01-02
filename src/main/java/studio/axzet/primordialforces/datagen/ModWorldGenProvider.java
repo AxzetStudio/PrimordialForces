c72997b22f36e0910e635189b65a6f3b225a7e1a
FJReyna <franciscorgs@hotmail.es>
2025-01-02 16:30:06 -0600

    Added worldgen setup

---
+++ src/main/java/studio/axzet/primordialforces/datagen/ModWorldGenProvider.java
@@ -0,0 +1,27 @@
+package studio.axzet.primordialforces.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+import studio.axzet.primordialforces.PrimordialForces;
+import studio.axzet.primordialforces.worldgen.ModBiomeModifier;
+import studio.axzet.primordialforces.worldgen.ModConfiguredFeatures;
+import studio.axzet.primordialforces.worldgen.ModPlacedFeatures;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+public class ModWorldGenProvider extends DatapackBuiltinEntriesProvider {
+
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifier::bootstrap);
+
+    public ModWorldGenProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries, BUILDER, Set.of(PrimordialForces.MOD_ID));
+    }
+}
