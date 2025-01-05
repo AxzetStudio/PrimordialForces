a42f5552a2efd9cb7dc2f4d850ea4d21bfddb93e
FJReyna <franciscorgs@hotmail.es>
2025-01-05 13:33:52 -0600

    Added void essence to undead mobs and refactorized

--- src/main/java/studio/axzet/primordialforces/event/ModEvents.java
+++ src/main/java/studio/axzet/primordialforces/event/ModEvents.java
@@ -1,7 +1,7 @@
 package studio.axzet.primordialforces.event;
 
 import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.entity.monster.*;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -12,16 +12,29 @@ import studio.axzet.primordialforces.item.ModItems;
 @EventBusSubscriber(modid = PrimordialForces.MOD_ID, bus = EventBusSubscriber.Bus.GAME)
 public class ModEvents {
 
-    static float dropChance = 0.3f;
+    private static float DROP_CHANCE = 0.3f;
 
     @SubscribeEvent
     public static void onLivingDrops(LivingDropsEvent event) {
-        if (event.getEntity() instanceof Zombie zombie) {
-            if (zombie.level().random.nextFloat() < dropChance) {
-                ItemStack voidEssence = new ItemStack(ModItems.VOID_ESSENCE.get());
-                ItemEntity drop = new ItemEntity(zombie.level(), zombie.getX(), zombie.getY(), zombie.getZ(), voidEssence);
-                event.getDrops().add(drop);
+        if (isVoidEligibleEntity(event.getEntity())) {
+            if (event.getEntity().level().random.nextFloat() < DROP_CHANCE) {
+                addVoidDrop(event);
             }
         }
+    }
+
+    private static boolean isVoidEligibleEntity(Object entity) {
+        return entity instanceof Zombie
+                || entity instanceof Skeleton
+                || entity instanceof Witch
+                || entity instanceof Stray
+                || entity instanceof Zoglin
+                ;
+    }
+
+    private static void addVoidDrop(LivingDropsEvent event) {
+        ItemStack voidEssence = new ItemStack(ModItems.VOID_ESSENCE.get());
+        ItemEntity drop = new ItemEntity(event.getEntity().level(), event.getEntity().getX(), event.getEntity().getY(), event.getEntity().getZ(), voidEssence);
+        event.getDrops().add(drop);
     }
 }
