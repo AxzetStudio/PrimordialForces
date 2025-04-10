35ac39815d1bf830f7bdd7948db23b0fca7c64a1
FJReyna <franciscorgs@hotmail.es>
2025-04-10 15:58:21 -0600

    Renamed thevoid dimension with creationforge

--- src/main/java/studio/axzet/primordialforces/portal/ModPortals.java
+++ src/main/java/studio/axzet/primordialforces/portal/ModPortals.java
@@ -8,31 +8,19 @@ import studio.axzet.primordialforces.block.ModBlocks;
 import studio.axzet.primordialforces.item.ModItems;
 
 public class ModPortals {
-    private static void voidDimensionPortal() {
+    private static void creationForgeDimensionPortal() {
         CustomPortalBuilder.beginPortal()
                 .frameBlock(ModBlocks.ARCADIUM_PORTAL_FRAME.get())
                 .lightWithItem(ModItems.VOID_SHARD.get())
                 .forcedSize(4, 5)
                 .onlyLightInOverworld()
-                .destDimID(ResourceLocation.fromNamespaceAndPath(PrimordialForces.MOD_ID, "thevoid"))
-                .registerPortal()
-        ;
-    }
-
-    private static void portalTest() {
-        CustomPortalBuilder.beginPortal()
-                .frameBlock(ModBlocks.BLACK_OPAL_BLOCK.get())
-                .lightWithItem(Items.DIAMOND)
-                .forcedSize(4, 5)
-                .onlyLightInOverworld()
-                .destDimID(ResourceLocation.parse("the_end"))
-                .tintColor(249, 248, 69)
+                .destDimID(ResourceLocation.fromNamespaceAndPath(PrimordialForces.MOD_ID, "creationforge"))
                 .registerPortal()
         ;
     }
 
     public static void createPortals() {
         //portalTest();
-        voidDimensionPortal();
+        creationForgeDimensionPortal();
     }
 }
