dd5d363b19edc78285095a40a17667e45680d2fb
FJReyna <franciscorgs@hotmail.es>
2025-05-04 16:51:13 -0600

    Fixed Arcadium Infuser voxel shape

--- src/main/java/studio/axzet/primordialforces/block/custom/ArcadiumInfuserBlock.java
+++ src/main/java/studio/axzet/primordialforces/block/custom/ArcadiumInfuserBlock.java
@@ -10,8 +10,11 @@ import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
@@ -20,16 +23,24 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 import studio.axzet.primordialforces.block.entity.ArcadiumInfuserBlockEntity;
 import studio.axzet.primordialforces.block.entity.ModBlockEntities;
 
 public class ArcadiumInfuserBlock extends BaseEntityBlock {
+    public static final VoxelShape SHAPE = Block.box(0, 0, 0, 16, 14, 16);
 
     public static final MapCodec<ArcadiumInfuserBlock> CODEC = simpleCodec(ArcadiumInfuserBlock::new);
 
     public ArcadiumInfuserBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE;
     }
 
     @Override
