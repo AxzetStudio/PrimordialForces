5eede0c3e8fd74c3ac33644c3e6fd1fc17a045ad
FJReyna <franciscorgs@hotmail.es>
2025-06-19 15:12:09 -0600

    Rearranged MossGolem goals

--- src/main/java/studio/axzet/primordialforces/entity/custom/MossGolemEntity.java
+++ src/main/java/studio/axzet/primordialforces/entity/custom/MossGolemEntity.java
@@ -1,7 +1,6 @@
 package studio.axzet.primordialforces.entity.custom;
 
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
@@ -14,6 +13,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
@@ -33,10 +33,10 @@ public class MossGolemEntity extends Animal implements GeoEntity {
 
     public static AttributeSupplier.Builder createAttributes() {
         return Animal.createMobAttributes()
-                .add(Attributes.FOLLOW_RANGE, 5.0)
+                .add(Attributes.FOLLOW_RANGE, 8.0)
                 .add(Attributes.MAX_HEALTH, 20)
                 .add(Attributes.ATTACK_DAMAGE, 6.0f)
-                .add(Attributes.ATTACK_SPEED, 0.5f)
+                .add(Attributes.ATTACK_SPEED, 0.25f)
                 .add(Attributes.ARMOR, 2.0)
                 .add(Attributes.MOVEMENT_SPEED, 0.2f);
     }
@@ -45,10 +45,9 @@ public class MossGolemEntity extends Animal implements GeoEntity {
     protected void registerGoals() {
         this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1, false) {
             @Override
-            protected void checkAndPerformAttack(LivingEntity target) {
+            protected void checkAndPerformAttack(@NotNull LivingEntity target) {
                 if (this.canPerformAttack(target)) {
                     triggerAnim("attack_controller", "attack1");
-                    this.mob.swing(InteractionHand.MAIN_HAND);
                     this.mob.doHurtTarget(target);
                     this.resetAttackCooldown();
                 }
@@ -64,12 +63,11 @@ public class MossGolemEntity extends Animal implements GeoEntity {
                 return 20;
             }
         });
-        this.goalSelector.addGoal(2, new MoveTowardsTargetGoal(this, 0.9, 32));
-        this.goalSelector.addGoal(4, new RandomStrollGoal(this, 1, 200));
-        this.goalSelector.addGoal(7, new LookAtPlayerGoal(this, Player.class, 4.0f));
-        this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(2, new WaterAvoidingRandomStrollGoal(this, 0.8));
+        this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 16.0f));
+        this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
 
-        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this,  Player.class, true));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this,  Player.class, true));
     }
 
     @Override
