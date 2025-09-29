967572f32c8767c2e1df29b0684cf002b6d992a7
FJReyna <franciscorgs@hotmail.es>
2025-09-29 14:15:39 -0600

    Added arcadium infuser emissive textures

--- src/main/java/studio/axzet/primordialforces/block/client/ArcadiumInfuserBlockEntityRenderer.java
+++ src/main/java/studio/axzet/primordialforces/block/client/ArcadiumInfuserBlockEntityRenderer.java
@@ -14,6 +14,7 @@ public class ArcadiumInfuserBlockEntityRenderer extends GeoBlockRenderer<Arcadiu
 
     public ArcadiumInfuserBlockEntityRenderer(BlockEntityRendererProvider.Context context) {
         super(new DefaultedBlockGeoModel<>(ResourceLocation.fromNamespaceAndPath(PrimordialForces.MOD_ID, "arcadium_infuser")));
+        this.addRenderLayer(new AutoGlowingGeoLayer<>(this));
     }
 
     @Override
